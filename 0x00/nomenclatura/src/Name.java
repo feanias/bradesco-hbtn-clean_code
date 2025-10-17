5c53495f9f0c0a09e4b5996c1cfc37bd0a7ce260
Felipe Anias <36409468+feanias@users.noreply.github.com>
2025-10-16 22:28:19 -0300

    0

---
+++ 0x00/nomenclatura/src/Name.java
@@ -0,0 +1,58 @@
+import java.net.URL;
+import java.util.Date;
+
+public class Name {
+    public Date modifiedDate;
+
+    public List<Testcase> findAllTestcasesByUser(Name.User user){
+        return null;
+    };
+
+    public List<Account> AccountList;
+
+    public void deleteUser(Name.User user){
+
+    };
+
+    public URL url;
+
+    public class TableRepresent {
+
+    };
+
+    public void saveCar(){
+
+    };
+
+    public void saveUser() {
+
+    }
+
+    public void saveAccount() {
+
+    }
+
+    public void saveContract() {
+
+    }
+
+
+    public static final int DAYS_IN_YEAR = 360;
+    public static final int HOURS_IN_WEEK = 168;
+
+
+    public static class User {
+
+    }
+
+    public static class Account {
+
+    }
+
+    public static class Testcase {
+
+    }
+
+
+
+}
